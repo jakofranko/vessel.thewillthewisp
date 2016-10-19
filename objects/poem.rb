c40c6d8a4c588815fac36edbeedade1c0322d3c9
Devine Lu Linvega <aliceffekt@gmail.com>
2016-10-19 15:37:15 -0700

    Various updates

--- objects/poem.rb
+++ objects/poem.rb
@@ -12,6 +12,7 @@ class Poem
 	def to_s
 
     @templates = Ra.new("templates",$instance_path).to_a
+    template   = @templates.sample
     @dict      = make_dict
 
     p = nil
@@ -19,40 +20,12 @@ class Poem
     while !p
       puts "Try #{c}"
       @usedWords = []
-      if c > 20 then targetTemplate = @templates.sample ; c = 0 end
-      p = generate(@templates.sample)
+      if c > 20 then template = @templates.sample ; c = 0 end
+      p = generate(template)
       c += 1
     end
 
-    p = p.gsub("A o", "An o")
-    p = p.gsub("A a", "An a")
-    p = p.gsub("A u", "An u")
-    p = p.gsub("A e", "An e")
-    p = p.gsub("A i", "An i")
-
-    p = p.gsub("a a", "an a")
-    p = p.gsub("a i", "an i")
-    p = p.gsub("a u", "an u")
-    p = p.gsub("a o", "an o")
-    p = p.gsub("a e", "an e")
-
-    p = p.gsub("a A", "an A")
-    p = p.gsub("a I", "an I")
-    p = p.gsub("a U", "an U")
-    p = p.gsub("a O", "an O")
-    p = p.gsub("a E", "an E")
-
-    p = p.gsub("A A", "An A")
-    p = p.gsub("A I", "An I")
-    p = p.gsub("A U", "An U")
-    p = p.gsub("A O", "An O")
-    p = p.gsub("A E", "An E")
-
-    p = p.gsub("a ha", "an ha")
-    p = p.gsub("sss","ses")
-    p = p.gsub!(/^\s*\w/){|match| match.upcase}
-
-    return p
+    return prettify(p)
 
   end
 
@@ -60,22 +33,22 @@ class Poem
 
     macros = template.scan(/(?:\{)([\w\W]*?)(?=\})/)
 
-    targ = [(@query ? @query.strip.split(" ").first : findTarget),findTarget,findTarget,findTarget,findTarget]
-    
+    targ_a = [(@query ? @query.strip.split(" ").first : findTarget),findTarget,findTarget,findTarget,findTarget]
+    sign_a = [">","<",">","="].shuffle
+    syll_a = [1,2,3,4].shuffle
+
+    c = 0
     macros.each do |macro,v|
-
       type = macro[0,1]
-      posi = macro[1,1]
-      team = macro[2,1].to_i
-      capi = type.capitalize == type ? true : false
-
-      game = findWord(type,posi,targ[team-1])
-      if game == nil then return nil end
-      game = capi == true ? game.capitalize : game
-      @usedWords.push(game.downcase)
-
-      template = template.sub("{#{macro}}",game)
-
+      team = macro[1,1].to_i
+      sign = team > 0 ? sign_a[team] : "?"
+      targ = targ_a[team-1]
+      syll = team > 0 ? syll_a[team] : nil
+      word = findWord(type,sign,targ,syll)
+      if !word then return nil end
+      template = template.sub("{#{macro}}",word)
+      @usedWords.push(word.downcase)
+      c += 1
     end
 
     return template.strip
@@ -93,22 +66,23 @@ class Poem
 
   end
 
-  def findWord type, posi, target
+  def findWord type, posi, target, syll
 
-    if    type.downcase == "n" then type_name = "NOUN"
-    elsif type.downcase == "v" then type_name = "VERB"
-    elsif type.downcase == "a" then type_name = "ADJ"
-    elsif type.downcase == "i" then type_name = "INTERROG"
-    elsif type.downcase == "p" then type_name = "PREP"
-    elsif type.downcase == "m" then type_name = "POSS"
-    elsif type.downcase == "d" then type_name = "ADV"
-    elsif type.downcase == "i" then type_name = "INTERROG"
+    if    type.like("n") then type_name = "NOUN"
+    elsif type.like("v") then type_name = "VERB"
+    elsif type.like("a") then type_name = "ADJ"
+    elsif type.like("i") then type_name = "INTERROG"
+    elsif type.like("p") then type_name = "PREP"
+    elsif type.like("m") then type_name = "ARTICLE"
+    elsif type.like("d") then type_name = "ADV"
+    elsif type.like("i") then type_name = "INTERROG"
     else return nil end
 
     @dict[type_name].shuffle.each do |word|
       if !word then next end
       if @usedWords.include?(word) then next end
-      if word.length < 4 then next end
+      if syll && count_syllables(word) != syll then next end
+      if word.length < 2 then next end
       if word.length > 9 then next end
       if posi == "=" && word == target then return word end
       if posi == "<" && word[0,3] == target[0,3] then return word end
@@ -117,6 +91,35 @@ class Poem
     end
 
     return nil
+
+  end
+
+  def prettify p
+
+    p = p.gsub("a a", "an a")
+    p = p.gsub("a i", "an i")
+    p = p.gsub("a u", "an u")
+    p = p.gsub("a o", "an o")
+    p = p.gsub("a e", "an e")
+
+    p = p.gsub("sss","sses")
+    p = p.gsub("shs","shes")
+    # p = p.gsub!(/^\s*\w/){|match| match.upcase}
+
+    p = p.gsub(";","\n")
+    p = p.capitalize
+
+    return p
+
+  end
+
+  def count_syllables target
+
+    target = target.to_s.downcase
+    return 1 if target.length <= 3
+    target = target.sub(/(?:[^laeiouy]es|ed|[^laeiouy]e)$/, '')
+    target = target.sub(/^y/, '')
+    return target.scan(/[aeiouy]{1,2}/).size
 
   end
 
