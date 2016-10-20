36068c7671744f5229518ad49143a03c99c950fb
Devine Lu Linvega <aliceffekt@gmail.com>
2016-10-20 11:48:22 -0700

    Rebuilt auto/tweet

--- vessel.rb
+++ vessel.rb
@@ -9,41 +9,59 @@ class Willw
 
   include Vessel
 
-  class PassiveActions
+  def twitter_account
+
+    return "thewillthewisp"
+
+  end
+
+  def make_poem q = nil
+
+    return Poem.new(q).to_s
+
+  end
+
+  # Actions
+
+  class Actions
 
     include ActionCollection
+    include ActionTweet
 
-    def answer q = nil
+    def tweet_auto
 
-      return Poem.new(q).to_s
+      return tweet(@actor.make_poem)
+
+    end
+
+    def tweet_reply_auto
+
+      last_reply  = last_replies.first
+      username    = last_reply.user.screen_name
+      target_word = last_reply.text.downcase.split(' ').last.gsub(/[^0-9a-z]/i, '')
+
+      # Check memory
+
+      ra = Ra.new("memory",$instance_path)
+      if ra.to_s == "#{username}:#{target_word}" then return "Already replied." end
+
+      # Make Poem
+
+      poem = @actor.make_poem(target_word)
+      if poem.to_s == ""
+        tweet_reply(last_reply,"@#{username} I cannot make you a rhyme from #{target_word}.",true)
+      else
+        tweet_reply(last_reply,"#{poem}\nThe #{target_word.capitalize}, for @#{username}.",true)
+      end
+
+      # Update memory
+      ra.replace("#{username}:#{target_word}")
+      return "Created poem: #{poem}"
 
     end
 
   end
-  
-  def passive_actions ; return PassiveActions.new(self,self) end
 
-  def poem_for username,word
-
-    poem = Poem.new(word).to_s
-
-    if poem.to_s == "" then return "@#{username} I cannot make you a rhyme from #{word}." end
-
-    ra = Ra.new("memory",$instance_path)
-    if ra.to_s == "#{username}:#{word}" then return nil end
-
-    ra.replace("#{username}:#{word}")
-
-    return "#{poem}\nThe #{word.capitalize}, for @#{username}."
-
-  end
-
-  # 
-
-  def display q = nil
-
-    return "- #{print.capitalize}, chirps a rhyme \"#{poem(q)}\".\n"
-
-  end
+  def actions ; return Actions.new(self,self) end
 
 end
